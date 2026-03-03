cf6486f6d5d2b8ac10510e137f1f1e35cb35166a
Henrique Bilhan <zepaya9@gmail.com>
2026-03-03 14:39:36 -0300

    fix(multiplicar): corrige a logica da função multiplicar release

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
  //Projeto GitFlow completo
+function somar(a, b) {
+  return a - b;
+}
+console.log('Soma:', somar(2, 3));
+//--------------------------------------------------------------------//
+function multiplicar(a, b) {
+  return a * 2;
+}
+console.log('Multiplicação:', multiplicar(4, 5));
+//--------------------------------------------------------------------//
+function multiplicar(a, b) {
+  return a + b; // bug para corrigir
+}
+//--------------------------------------------------------------------//
+function multiplicar(a, b) {
+  return a * b;
+}
